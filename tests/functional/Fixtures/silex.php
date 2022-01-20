df09b4306b73549a26163a0455029a14db3092ef
Benjamin Doherty <bdoherty@activecampaign.com>
2022-01-20 01:25:14 -0600

    Replace silex so we can upgrade kernel.

--- tests/functional/Fixtures/silex.php
+++ tests/functional/Fixtures/silex.php
@@ -2,30 +2,68 @@
 
 require __DIR__.'/../../../vendor/autoload.php';
 
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\EventListener\ErrorListener;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-$app = new Silex\Application();
+$kernel = new class('test', true) extends Kernel {
+    use MicroKernelTrait;
+    public function registerBundles(): iterable { yield new FrameworkBundle(); }
+    private function getConfigDir(): string { return $this->getProjectDir(); }
 
-$app['exception_handler']->disable();
+    private function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routes
+            ->add('foo', '/foo')
+            ->methods(['GET'])
+            ->controller(['kernel', 'foo'])
+        ;
 
-$app->get('/foo', function () {
-    return 'bar';
-});
+        $routes
+            ->add('baz', '/baz')
+            ->methods(['POST'])
+            ->controller(['kernel', 'baz'])
+        ;
 
-$app->post('/baz', function () {
-    return 'qux';
-});
+        $routes
+            ->add('put-target', '/put-target')
+            ->methods(['PUT'])
+            ->controller(['kernel', 'putTarget'])
+        ;
 
-$app->put('/put-target', function () {
-    return 'putted';
-});
+        $routes
+            ->add('delete-target', '/delete-target')
+            ->methods(['DELETE'])
+            ->controller(['kernel', 'deleteTarget'])
+        ;
 
-$app->delete('/delete-target', function () {
-    return 'deleted';
-});
+        $routes
+            ->add('script-name', '/script-name')
+            ->methods(['GET'])
+            ->controller(['kernel', 'scriptName'])
+        ;
+    }
 
-$app->get('/script-name', function (Request $request) {
-    return $request->server->get('SCRIPT_NAME');
-});
+    private function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $container->services()->remove(ErrorListener::class);
+    }
 
-$app->run();
+    public function foo() { return new Response('bar'); }
+    public function baz() { return new Response('qux'); }
+    public function putTarget() { return new Response('putted'); }
+    public function deleteTarget() { return new Response('deleted'); }
+    public function scriptName(Request $request) { return new Response($request->server->get('SCRIPT_NAME')); }
+};
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
